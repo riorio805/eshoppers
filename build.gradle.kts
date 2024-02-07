add8e5592a8aedfff50bf1868c4de9f7841205b2
riorio805 <sefrianojieftara@gmail.com>
2024-02-07 17:45:51 +0800

    tests config

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-
 }
 
 tasks.register<Test>("unitTest") {
