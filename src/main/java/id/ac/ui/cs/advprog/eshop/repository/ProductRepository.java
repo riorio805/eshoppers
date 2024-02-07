757871734ca66cfc57c68464f6842c98475f2bbb
riorio805 <sefrianojieftara@gmail.com>
2024-02-07 17:09:32 +0800

    get and set product in ProductRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,17 +3,34 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private final List<Product> productData = new ArrayList<>();
+    private final Map<String, Product> productMap = new HashMap<>();
 
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
+        productMap.put(product.getProductId(), product);
         return product;
+    }
+
+    public Product get(String productId) {
+        return productMap.get(productId);
+    }
+
+    // change oldProduct to match newProduct
+    public void edit(Product newProduct) {
+        Product oldProduct = productMap.get(newProduct.getProductId());
+        oldProduct.setProductQuantity(newProduct.getProductQuantity());
+        oldProduct.setProductName(newProduct.getProductName());
     }
 
     public Iterator<Product> findAll() {
