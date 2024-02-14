4c665d9c717731fdaf0beb05761d273768a7f4a1
riorio805 <sefrianojieftara@gmail.com>
2024-02-14 17:29:41 +0800

    fix unnecessary imports in MainPageController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainPageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainPageController.java
@@ -4,7 +4,8 @@ import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
