c366de750c317135cd3a3373c9c428f4a1c3f68c
riorio805 <sefrianojieftara@gmail.com>
2024-02-15 14:30:48 +0800

    Commit CarRepository.java to your Git repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,12 +3,12 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.UUID;
 
 @Repository
 public class ProductRepository {
