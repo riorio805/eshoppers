35c3fdbde65e3b36ea9f025ccd5af092e3362f29
riorio805 <sefrianojieftara@gmail.com>
2024-02-29 23:10:50 +0700

    [RED] fix PaymentTest (no3 pull out unnecessary information from each test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -14,43 +14,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PaymentTest {
     private List<Product> products;
+    private Order order;
 
     @BeforeEach
     void setUp() {
         this.products = new ArrayList<>();
         Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductId("ca8badb9-b79c-a26f-edc3-3010705ec774");
         product1.setProductName("Sampo Mantap Bang");
         product1.setProductQuantity(5);
         this.products.add(product1);
         Product product2 = new Product();
-        product2.setProductId("dd57b860-7612-4b75-86f7-f625f53907bd");
+        product2.setProductId("ca8badb9-b79c-a26f-c10c-420f6bd6f739");
         product2.setProductName("Es Krim Esdea");
         product2.setProductQuantity(1);
         this.products.add(product2);
         Product product3 = new Product();
-        product3.setProductId("0c1ce873-59e6-461e-9741-7bee2cd95228");
-        product3.setProductName("Algoritma Convolusi");
+        product3.setProductId("ae2b2d7a-4962-9e4f-dbd2-a17d1244c53f");
+        product3.setProductName("ristler juice");
         product3.setProductQuantity(2);
         this.products.add(product3);
+
+        this.order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, 1708690000L, "Dek Depe");
     }
 
     @Test
     void testPaymentValid() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-            this.products, 1708690000L, "Dek Depe");
-
+        // Valid payment data
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
         Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
                 "VOUCHER_CODE", order, paymentData);
-        assertSame(order.getProducts(), payment.getOrder().getProducts());
         assertEquals(order.getProducts().size(), payment.getOrder().getProducts().size());
-        assertEquals(this.products.get(0).getProductName(),
-                payment.getOrder().getProducts().get(0).getProductName());
-        assertEquals(this.products.get(1).getProductName(),
-                payment.getOrder().getProducts().get(1).getProductName());
+        assertEquals(this.products.get(1).getProductId(),
+                payment.getOrder().getProducts().get(1).getProductId());
+        assertEquals(this.products.get(2).getProductId(),
+                payment.getOrder().getProducts().get(2).getProductId());
 
         assertEquals("21a11451-cc2c-4033-bd88-48fbb04032e7", payment.getId());
         assertSame(paymentData, payment.getPaymentData());
@@ -58,37 +59,29 @@ public class PaymentTest {
     }
 
     @Test
-    void testPaymentOrderAlreadyCompleted() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-            this.products, 1708690000L, "Dek Depe");
-
+    void testPaymentFailIfOrderAlreadyCompleted() {
+        Payment payment;
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
         order.setStatus(OrderStatus.SUCCESS.getValue());
-        assertThrows(IllegalArgumentException.class, () -> {
-            Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
-                "VOUCHER_CODE", order, paymentData);
-        });
+        payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
+            "VOUCHER_CODE", order, paymentData);
+        assertEquals("REJECTED", payment.getStatus());
 
         order.setStatus(OrderStatus.CANCELLED.getValue());
-        assertThrows(IllegalArgumentException.class, () -> {
-            Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
-                "VOUCHER_CODE", order, paymentData);
-        });
+        payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
+            "VOUCHER_CODE", order, paymentData);
+        assertEquals("REJECTED", payment.getStatus());
 
         order.setStatus(OrderStatus.FAILED.getValue());
-        assertThrows(IllegalArgumentException.class, () -> {
-            Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
-                "VOUCHER_CODE", order, paymentData);
-        });
+        payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
+            "VOUCHER_CODE", order, paymentData);
+        assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testPaymentInvalidPaymentMethod() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
@@ -100,40 +93,44 @@ public class PaymentTest {
 
     @Test
     void testValidVoucherCode() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
         Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
             "VOUCHER_CODE", order, paymentData);
 
-        assertEquals("21a11451-cc2c-4033-bd88-48fbb04032e7", payment.getId());
-        assertEquals(order, payment.getOrder());
-        assertEquals(paymentData, payment.getPaymentData());
         assertEquals("SUCCESS", payment.getStatus());
     }
 
     @Test
-    void testVoucherCodeWrongLength() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
+    void testVoucherCodeEmptyData() {
         Map<String, String> paymentData = new HashMap<>();
-        paymentData.put("voucherCode", "ESHOP26022024");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
+                "VOUCHER_CODE", order, paymentData);
+        });
+    }
+
+    @Test
+    void testVoucherCodeWrongLength() {
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOPaa89012345");
 
         Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
             "VOUCHER_CODE", order, paymentData);
+        assertEquals("REJECTED", payment.getStatus());
 
+        paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOPabcd01234567");
+
+        payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
+                "VOUCHER_CODE", order, paymentData);
         assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
     void testVoucherCodeWrongPrefix() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "PHOSE1234ABC5678");
 
@@ -145,16 +142,12 @@ public class PaymentTest {
 
     @Test
     void testVoucherCodeWrongNumberCount() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "PHOSE1234ABC567P");
 
         Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
                 "VOUCHER_CODE", order, paymentData);
         assertEquals("REJECTED", payment.getStatus());
-
 
         paymentData = new HashMap<>();
         paymentData.put("voucherCode", "PHOSE1234AB56789");
@@ -166,9 +159,6 @@ public class PaymentTest {
 
     @Test
     void testBankTransferValid() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("bankName", "STM Bank");
         paymentData.put("referenceCode", "82un3rc0");
@@ -176,17 +166,11 @@ public class PaymentTest {
         Payment payment = new Payment("21a11451-cc2c-4033-bd88-48fbb04032e7",
                 "BANK_TRANSFER", order, paymentData);
 
-        assertEquals("21a11451-cc2c-4033-bd88-48fbb04032e7", payment.getId());
-        assertEquals(order, payment.getOrder());
-        assertEquals(paymentData, payment.getPaymentData());
         assertEquals("SUCCESS", payment.getStatus());
     }
 
     @Test
-    void testBankTransferInvalidBankName() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
+    void testBankTransferEmptyBankName() {
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("bankName", "");
         paymentData.put("referenceCode", "e80fuv32");
@@ -205,10 +189,7 @@ public class PaymentTest {
     }
 
     @Test
-    void testBankTransferInvalidRefCode() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708690000L, "Dek Depe");
-
+    void testBankTransferEmptyRefCode() {
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("bankName", "Bank Mandiri");
         paymentData.put("referenceCode", "");
