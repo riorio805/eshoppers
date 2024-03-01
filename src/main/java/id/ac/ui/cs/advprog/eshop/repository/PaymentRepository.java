c4bd81bef6f06668aad65ebbb27f527d15722409
riorio805 <sefrianojieftara@gmail.com>
2024-03-01 11:31:39 +0700

    [GREEN] all PaymentRepositoryTest green

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
@@ -11,22 +14,36 @@ public class PaymentRepository {
     private final List<Payment> paymentData = new ArrayList<>();
 
     public Payment addPayment(Payment payment) {
-        return null;
+        paymentData.add(payment);
+        return payment;
     }
 
-    public Payment findById(String paymentId) {
+    public Payment findById(String id) {
+        for (Payment savedPayment: paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public Payment setStatus(Payment payment, String status) {
-        return null;
-    }
+        if (! PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        payment.updateStatus(status);
 
-    public Payment setStatusWithId(String paymentId, String status) {
-        return null;
+        if ( payment.getStatus().equals(PaymentStatus.SUCCESS.getValue()) ) {
+            payment.getOrder().setStatus(OrderStatus.SUCCESS.getValue());
+        }
+        else if ( payment.getStatus().equals(PaymentStatus.REJECTED.getValue()) ) {
+            payment.getOrder().setStatus(OrderStatus.FAILED.getValue());
+        }
+
+        return payment;
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentData;
     }
 }
